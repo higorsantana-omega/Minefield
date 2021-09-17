d553f63ef1a1318b31a217a5690c51b0d9e38028
higorsantana-omega <70420430+higorsantana-omega@users.noreply.github.com>
2021-09-17 16:20:11 -0300

    feat: logic to insert flag

--- src/logicGame.js
+++ src/logicGame.js
@@ -98,6 +98,11 @@ const showMines = (board) =>
     .filter((field) => field.mined)
     .forEach((field) => (field.opened = true));
 
+const invertFlag = (board, row, column) => {
+  const field = board[row][column];
+  field.flagged = !field.flagged;
+};
+
 export {
   createMinedBoard,
   cloneBoard,
@@ -105,4 +110,5 @@ export {
   hadExplosion,
   wonGame,
   showMines,
+  invertFlag,
 };
